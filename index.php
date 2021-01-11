0b9c7a7305cd0cd7f6f9da974f5c726f6fdbefa3
Theeradon B <gufilmsamui@users.noreply.github.com>
2021-01-11 15:54:06 +0700

    icon

--- index.php
+++ index.php
@@ -7,12 +7,13 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="icon/css/all.css">
     <script src="js/default.js"></script>
     <link rel="stylesheet" href="css/mystyle.css">
 </head>
 <body>
     <div class="container">
-        Hello World 
+        <i class="fab fa-accessible-icon"></i>
         <button class="btn btn-primary">xxx</button>
         <button class="btn btn-primary">xxx</button>
         <img src="img/1200px-Tree_Sparrow_Japan_Flip.jpg" alt="">
